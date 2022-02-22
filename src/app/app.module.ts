466b9e9c9f85ee10e2fc9838f191ef09806272b9
Christian Moll <11926959+ChristianMoll@users.noreply.github.com>
2022-02-22 18:40:11 +0100

    wip
    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SlidingDirective } from './sliding.directive';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidingDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sliding.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SlidingDirective } from './sliding.directive';
+
+describe('SlidingDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SlidingDirective();
+    expect(directive).toBeTruthy();
+  });
+});
